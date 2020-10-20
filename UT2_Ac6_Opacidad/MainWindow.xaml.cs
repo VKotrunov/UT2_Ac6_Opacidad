dca26f2bc72ff8330e9e49f1aa83a1024742c364
VKotrunov <57618856+VKotrunov@users.noreply.github.com>
2020-10-20 12:15:10 +0200

    Agregar archivos de proyecto.

---
+++ UT2_Ac6_Opacidad/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UT2_Ac6_Opacidad
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 1;
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 0.5;
+        }
+    }
+}
